dbc590721019a69dc74ffe7656e37e0a3eda0907
Edan Bainglass <edan.bainglass@gmail.com>
2026-06-18 05:48:04 +0200

    Discard default projections

--- src/App.tsx
+++ src/App.tsx
@@ -115,11 +115,6 @@ const App = () => {
       {} as Record<string, string[]>,
     );
 
-    const lastTag = path[path.length - 1].tag;
-    if (Object.values(project).every((p) => p.length === 0)) {
-      project[lastTag] = ["**"];
-    }
-
     return {
       path,
       filters,
